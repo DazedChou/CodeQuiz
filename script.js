ea07ad57ef4fe7bf6bd86d2c592dd26bd185c8e7
David Chou <davidchou2009@gmail.com>
2021-12-26 18:33:11 -0800

    set up basic element containers and variables in js

---
+++ script.js
@@ -0,0 +1,7 @@
+quizEl = document.querySelector("#quiz");
+resultsEl = document.querySelector("#results");
+startEl = document.querySelector("#start");
+submitEl = document.querySelector("#submit");
+
+var timer = 60;
+
