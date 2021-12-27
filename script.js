9264e6bac3d50d339ada8ed161f5dd7c0d95d373
David Chou <davidchou2009@gmail.com>
2021-12-26 19:47:24 -0800

    add quiz questions

--- script.js
+++ script.js
@@ -4,4 +4,77 @@ startEl = document.querySelector("#start");
 submitEl = document.querySelector("#submit");
 
 var timer = 60;
+var score = 0;
 
+//create array of object of questions and corresponding answers
+var questions = [
+    {
+        question: "Commonly used data types DO NOT include: ",
+        answers: {
+            a: "strings",
+            b: "booleans",
+            c: "alerts",
+            d: "numbers"
+        },
+        answer: "c"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within _______.",
+        answers: {
+            a: "quotes",
+            b: "curly brackets",
+            c: "parentheses",
+            d: "square brackets"
+        },
+        correct: "c"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store _______.",
+        answers: {
+            a: "numbers and strings",
+            b: "other arrays",
+            c: "booleans",
+            d: "all of the above"
+        },
+        correct: "d"
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        answers: {
+            a: "commas",
+            b: "curly brackets",
+            c: "quotes",
+            d: "parenthesis"
+        },
+        correct: "c"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is: ",
+        answers: {
+            a: "Javascript",
+            b: "console.log",
+            c: "terminal/bash",
+            d: "for loops"
+        },
+        correct: "b"
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: {
+            a: "<js>",
+            b: "<script>",
+            c: "<javascript>",
+            d: "<scripting>"
+        },
+        correct: "b"
+    },
+]
+
+
+function startGame(){
+    
+}
+
+function displayResults(){
+
+}
