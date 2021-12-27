d3b9fad4b1924fd5faf5f9b2e2d85fca16a8ab29
David Chou <davidchou2009@gmail.com>
2021-12-26 22:11:05 -0800

    display question as quiz starts

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var quizEl = document.querySelector("#quiz");
 var resultsEl = document.querySelector("#results");
 var startEl = document.querySelector("#start");
 var submitEl = document.querySelector("#submit");
+var choicesEl = document.querySelector("#choiceSelection");
 
 var timer = 60;
 var score = 0;
@@ -74,7 +75,11 @@ console.log(quizEl.children[0]);
 
 
 function startGame(){
-    startEl.textContent = "Next";
+
+    document.querySelector(".button").removeChild(startEl);
+    var nextQuestion = document.createElement("button");
+    document.querySelector(".button").appendChild(nextQuestion);
+    // startEl.textContent = "Next";
     var countdown = document.createElement("div");
     
     quizEl.appendChild(countdown);
@@ -93,6 +98,26 @@ function startGame(){
     }, 1000);
 
     //Display Quiz Questions
+    var choices = document.createElement("div");
+    var choiceA = document.createElement("button");
+    var choiceB = document.createElement("button");
+    var choiceC = document.createElement("button");
+    var choiceD = document.createElement("button");
+
+    choicesEl.appendChild(choiceA);
+    choicesEl.appendChild(choiceB);
+    choicesEl.appendChild(choiceC);
+    choicesEl.appendChild(choiceD);
+
+    for(var i = 0; i < questions.length ; i++){
+        quizEl.children[0].textContent = questions[i].question;
+        choiceA.textContent = questions[i].answers.a;
+        choiceB.textContent = questions[i].answers.b;
+        choiceC.textContent = questions[i].answers.c;
+        choiceD.textContent = questions[i].answers.d;
+
+    }
+
 
 }
 
