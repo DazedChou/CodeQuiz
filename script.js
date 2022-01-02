cc99b716258f79b8ea930d777c13fd189d33ce66
David Chou <davidchou2009@gmail.com>
2022-01-01 19:25:04 -0800

    save user initial and score

--- script.js
+++ script.js
@@ -4,10 +4,9 @@ var startEl = document.querySelector("#start");
 // var submitEl = document.querySelector("#submit");
 var choicesEl = document.querySelector("#choiceSelection");
 var timeEl = document.querySelector("#timeLeft");
+var hiscoreEl = document.querySelector("#highScores");
 
-var timer = 60; //set time length of quiz
-var score = 0; //user score
-var name =[]; //empty array to store names and scores
+var name = []; //empty array to store names and scores
 
 
 var questions = [
@@ -73,255 +72,253 @@ var questions = [
     },
 ];
 
-//Create button choices with unique id
-var choiceA = document.createElement("button");
-choiceA.setAttribute("id","choiceA")
-var choiceB = document.createElement("button");
-choiceB.setAttribute("id","choiceB")
-var choiceC = document.createElement("button");
-choiceC.setAttribute("id","choiceC")
-var choiceD = document.createElement("button");
-choiceD.setAttribute("id","choiceD")
 
-function startGame(){
+
+function createQuiz() {
+    var timer = 60; //set time length of quiz
+    var score = 0; //user score
     var i = 0; //index to track current question
 
-    document.querySelector(".button").removeChild(startEl);
-
-    quizEl.children[1].textContent=""; //removing p content
-
-    //create timer and update time every second
-    var timerInterval = setInterval(function() {
-        
-        timer--;
-        timeEl.textContent = "Time Left: " + timer;
-
-        if(timer === 0 || timer <=0){
-            timer = 0; //if timer is at 5 seconds, and user gets question wrong, timer 
-            //can become negative.
-            clearInterval(timerInterval);
-            endGame(); //end game when timer reaches 0
-            //Go display results
-        }
-    }, 1000);
-
     //Create button choices with unique id
-    // var choiceA = document.createElement("button");
-    // choiceA.setAttribute("id","choiceA")
-    // var choiceB = document.createElement("button");
-    // choiceB.setAttribute("id","choiceB")
-    // var choiceC = document.createElement("button");
-    // choiceC.setAttribute("id","choiceC")
-    // var choiceD = document.createElement("button");
-    // choiceD.setAttribute("id","choiceD")
+    var choiceA = document.createElement("button");
+    choiceA.setAttribute("id", "choiceA")
+    var choiceB = document.createElement("button");
+    choiceB.setAttribute("id", "choiceB")
+    var choiceC = document.createElement("button");
+    choiceC.setAttribute("id", "choiceC")
+    var choiceD = document.createElement("button");
+    choiceD.setAttribute("id", "choiceD")
 
-    choicesEl.appendChild(choiceA);
-    choicesEl.appendChild(choiceB);
-    choicesEl.appendChild(choiceC);
-    choicesEl.appendChild(choiceD);
-    console.log(choicesEl);
-    //Display quiz questions
- 
-    quizEl.children[0].textContent = questions[i].question;
-    choiceA.textContent = "1." + questions[i].answers.a;
-    choiceB.textContent = "2." +  questions[i].answers.b;
-    choiceC.textContent = "3." +  questions[i].answers.c;
-    choiceD.textContent = "4." +  questions[i].answers.d;
+    function startGame() {
+        console.log(document.querySelector(".button"));
+        document.querySelector(".button").removeChild(startEl);
+        // startEl.textContent = "";
+
+        //quizEl.children[1].textContent = ""; //removing p content
+        quizEl.children[1].setAttribute("style","display:none");
+        //create timer and update time every second
+        var timerInterval = setInterval(function () {
+
+            timer--;
+            timeEl.textContent = "Time Left: " + timer;
+
+            if (timer === 0 || timer <= 0) {
+                timer = 0; //if timer is at 5 seconds, and user gets question wrong, timer 
+                //can become negative.
+                clearInterval(timerInterval);
+                endGame(); //end game when timer reaches 0
+                //Go display results
+            }
+        }, 1000);
+
+
+        choicesEl.appendChild(choiceA);
+        choicesEl.appendChild(choiceB);
+        choicesEl.appendChild(choiceC);
+        choicesEl.appendChild(choiceD);
+        console.log(choicesEl);
+        //Display quiz questions
+
+        quizEl.children[0].textContent = questions[i].question;
+        choiceA.textContent = "1." + questions[i].answers.a;
+        choiceB.textContent = "2." + questions[i].answers.b;
+        choiceC.textContent = "3." + questions[i].answers.c;
+        choiceD.textContent = "4." + questions[i].answers.d;
 
 
 
-    choiceA.addEventListener("click",function(){
+        choiceA.addEventListener("click", function () {
 
-        //Display Next Question
-        if(i<6){
-            i++;
-        }
+            //Display Next Question
+            if (i < 6) {
+                i++;
+            }
 
 
-        //Set condition to end game when last question has been answered
-        if(i == 6){
-            endGame();
-            clearInterval(timerInterval)
-        }else{
-            quizEl.children[0].textContent = questions[i].question;
-            choiceA.textContent = "1." + questions[i].answers.a;
-            choiceB.textContent = "2." +  questions[i].answers.b;
-            choiceC.textContent = "3." +  questions[i].answers.c;
-            choiceD.textContent = "4." +  questions[i].answers.d;
-        }
-        //there is no question with the correct choice of A,
-        //so whenever user chooses A, time gets deducted by 10.
-        timer = timer - 10;
-
-    });
-    choiceB.addEventListener("click",function(){
-
-        //conditional to check for answer
-        if(i < 4){
+            //Set condition to end game when last question has been answered
+            if (i == 6) {
+                endGame();
+                clearInterval(timerInterval)
+            } else {
+                quizEl.children[0].textContent = questions[i].question;
+                choiceA.textContent = "1." + questions[i].answers.a;
+                choiceB.textContent = "2." + questions[i].answers.b;
+                choiceC.textContent = "3." + questions[i].answers.c;
+                choiceD.textContent = "4." + questions[i].answers.d;
+            }
+            //there is no question with the correct choice of A,
+            //so whenever user chooses A, time gets deducted by 10.
             timer = timer - 10;
-        }else{
-            score++;
-        }
-        
-        //Display Next Question
-        if(i<6){
-            i++;
-        }
 
-        //Set condition to end game when last question has been answered
-        if(i == 6){
-            endGame();
-            clearInterval(timerInterval)
-        }else{
-            quizEl.children[0].textContent = questions[i].question;
-            choiceA.textContent = "1." + questions[i].answers.a;
-            choiceB.textContent = "2." +  questions[i].answers.b;
-            choiceC.textContent = "3." +  questions[i].answers.c;
-            choiceD.textContent = "4." +  questions[i].answers.d;
-        }
+        });
+        choiceB.addEventListener("click", function () {
 
-    });
-    choiceC.addEventListener("click",function(){
+            //conditional to check for answer
+            if (i < 4) {
+                timer = timer - 10;
+            } else {
+                score++;
+            }
 
-        //conditional to check for answer
-        if(i == 2 || i == 4 || i == 5){
-            timer = timer - 10;
-        }else{
-            score++;
-        }
+            //Display Next Question
+            if (i < 6) {
+                i++;
+            }
 
-        //Display Next Question
-        if(i<6){
-            i++;
-        }
-        
-        //Set condition to end game when last question has been answered
-        if(i == 6){
-            clearInterval(timerInterval)
-            endGame();
-        }else{
-            quizEl.children[0].textContent = questions[i].question;
-            choiceA.textContent = "1." + questions[i].answers.a;
-            choiceB.textContent = "2." +  questions[i].answers.b;
-            choiceC.textContent = "3." +  questions[i].answers.c;
-            choiceD.textContent = "4." +  questions[i].answers.d;
-        }
+            //Set condition to end game when last question has been answered
+            if (i == 6) {
+                endGame();
+                clearInterval(timerInterval)
+            } else {
+                quizEl.children[0].textContent = questions[i].question;
+                choiceA.textContent = "1." + questions[i].answers.a;
+                choiceB.textContent = "2." + questions[i].answers.b;
+                choiceC.textContent = "3." + questions[i].answers.c;
+                choiceD.textContent = "4." + questions[i].answers.d;
+            }
 
-    });
-    choiceD.addEventListener("click",function(){
+        });
+        choiceC.addEventListener("click", function () {
 
-        //conditional to check for answer
-        if(i == 2 ){
-            score++;
-        }else{
-            timer = timer - 10;
-        }
+            //conditional to check for answer
+            if (i == 2 || i == 4 || i == 5) {
+                timer = timer - 10;
+            } else {
+                score++;
+            }
 
-        //Display Next Question
-        if(i<6){
-            i++;
-        }
+            //Display Next Question
+            if (i < 6) {
+                i++;
+            }
 
-        //Set condition to end game when last question has been answered
-        if(i == 6){
-            clearInterval(timerInterval);
-            endGame();
-        }else{
-            quizEl.children[0].textContent = questions[i].question;
-            choiceA.textContent = "1." + questions[i].answers.a;
-            choiceB.textContent = "2." +  questions[i].answers.b;
-            choiceC.textContent = "3." +  questions[i].answers.c;
-            choiceD.textContent = "4." +  questions[i].answers.d;
-        }
+            //Set condition to end game when last question has been answered
+            if (i == 6) {
+                clearInterval(timerInterval)
+                endGame();
+            } else {
+                quizEl.children[0].textContent = questions[i].question;
+                choiceA.textContent = "1." + questions[i].answers.a;
+                choiceB.textContent = "2." + questions[i].answers.b;
+                choiceC.textContent = "3." + questions[i].answers.c;
+                choiceD.textContent = "4." + questions[i].answers.d;
+            }
 
-    });
+        });
+        choiceD.addEventListener("click", function () {
+
+            //conditional to check for answer
+            if (i == 2) {
+                score++;
+            } else {
+                timer = timer - 10;
+            }
+
+            //Display Next Question
+            if (i < 6) {
+                i++;
+            }
+
+            //Set condition to end game when last question has been answered
+            if (i == 6) {
+                clearInterval(timerInterval);
+                endGame();
+            } else {
+                quizEl.children[0].textContent = questions[i].question;
+                choiceA.textContent = "1." + questions[i].answers.a;
+                choiceB.textContent = "2." + questions[i].answers.b;
+                choiceC.textContent = "3." + questions[i].answers.c;
+                choiceD.textContent = "4." + questions[i].answers.d;
+            }
+
+        });
+
+    }
+    function endGame() {
+        //When game ends, remove the buttons and update text
+        choicesEl.removeChild(document.getElementById("choiceA"));
+        choicesEl.removeChild(document.getElementById("choiceB"));
+        choicesEl.removeChild(document.getElementById("choiceC"));
+        choicesEl.removeChild(document.getElementById("choiceD"));
+
+
+        document.querySelector("body").children[0].children[0].textContent = "All Done";
+        quizEl.children[0].textContent = "";
+
+        //create textbox and prompt user to enter initials
+        resultsEl.children[0].textContent = "Enter your initials below";
+        var textbox = document.createElement("input");
+        textbox.setAttribute("type", "text");
+        resultsEl.appendChild(textbox);
+
+        //create submit button
+        var submit = document.createElement("button");
+        resultsEl.appendChild(submit);
+        submit.textContent = "Submit"
+
+        submit.addEventListener("click", function () {
+            console.log(textbox.value);
+            document.querySelector("body").children[0].children[0].textContent = "High Scores";
+            resultsEl.children[0].textContent = "";
+            localStorage.setItem(textbox.value, score);
+
+            //create list for hi scores
+            resultsEl.removeChild(textbox);
+            resultsEl.removeChild(submit);
+            var listEl = document.createElement("ul");
+            var li1 = document.createElement("li");
+            var li2 = document.createElement("li");
+
+
+            resultsEl.appendChild(listEl);
+            listEl.appendChild(li1);
+            listEl.appendChild(li2);
+
+            li1.textContent = textbox.value + localStorage.getItem(textbox.value);
+
+            var restartGame = document.createElement("button");
+            restartGame.textContent = "Play again";
+            resultsEl.appendChild(restartGame);
+            restartGame.addEventListener("click", createQuiz);
+
+
+            // hiscores();
+        });
+
+
+    }
+
+    startGame();
 
 }
-function endGame(){
-    //When game ends, remove the buttons and update text
-    choicesEl.removeChild(document.getElementById("choiceA"));
-    choicesEl.removeChild(document.getElementById("choiceB"));
-    choicesEl.removeChild(document.getElementById("choiceC"));
-    choicesEl.removeChild(document.getElementById("choiceD"));
-
-    document.querySelector("body").children[0].children[0].textContent = "All Done";
-    quizEl.children[0].textContent = "";
-
-    //prompt user to enter initials in text box
-    resultsEl.children[0].textContent = "Enter your initials below";
-    var textbox = document.createElement("input");
-    textbox.setAttribute("type","text");
-    resultsEl.appendChild(textbox);
-
-    var submit = document.createElement("button");
-    resultsEl.appendChild(submit);
-    submit.textContent = "Submit"
-
-    submit.addEventListener("click",function(){
-        this.preventDefault();
-
-        name.push(textbox.value);
-        console.log(name);
-        localStorage.setItem(textbox.value,score);
-
-        //create list for hi scores
-        resultsEl.removeChild(textbox);
-        resultsEl.removeChild(submit);
-        var listEl = document.createElement("ul");
-        var li1 = document.createElement("li");
-        var li2 = document.createElement("li");
-        var li3 = document.createElement("li");
-        var li4 = document.createElement("li");
-        var li5 = document.createElement("li");
-        var li6 = document.createElement("li");
-    
-        resultsEl.appendChild(listEl);
-        listEl.appendChild(li1);
-        listEl.appendChild(li2);
-        listEl.appendChild(li3);
-        listEl.appendChild(li4);
-        listEl.appendChild(li5);
-        listEl.appendChild(li6);
-    
-        li1 = localStorage.getItem(name[0])
-    
-        var restartGame = document.createElement("button");
-        restartGame.textContent = "Play again";
-        resultsEl.appendChild(restartGame);
-        restartGame.addEventListener("click",startGame());
 
 
-        // hiscores();
-    });
 
+function hiscores() {
+    // resultsEl.removeChild(textbox);
+    // resultsEl.removeChild(submit);
+    // var listEl = document.createElement("ul");
+    // var li1 = document.createElement("li");
+    // var li2 = document.createElement("li");
+    // var li3 = document.createElement("li");
+    // var li4 = document.createElement("li");
+    // var li5 = document.createElement("li");
+    // var li6 = document.createElement("li");
 
-}
-
-function hiscores(){
-    resultsEl.removeChild(textbox);
-    resultsEl.removeChild(submit);
-    var listEl = document.createElement("ul");
-    var li1 = document.createElement("li");
-    var li2 = document.createElement("li");
-    var li3 = document.createElement("li");
-    var li4 = document.createElement("li");
-    var li5 = document.createElement("li");
-    var li6 = document.createElement("li");
-
-    resultsEl.appendChild(listEl);
-    listEl.appendChild(li1);
-    listEl.appendChild(li2);
-    listEl.appendChild(li3);
-    listEl.appendChild(li4);
-    listEl.appendChild(li5);
-    listEl.appendChild(li6);
+    // resultsEl.appendChild(listEl);
+    // listEl.appendChild(li1);
+    // listEl.appendChild(li2);
+    // listEl.appendChild(li3);
+    // listEl.appendChild(li4);
+    // listEl.appendChild(li5);
+    // listEl.appendChild(li6);
 
     li1 = localStorage.getItem(name[0])
 
     var restartGame = document.createElement("button");
     restartGame.textContent = "Play again";
     resultsEl.appendChild(restartGame);
-    restartGame.addEventListener("click",startGame());
+    restartGame.addEventListener("click", startGame());
 
 }
 
@@ -332,9 +329,6 @@ function hiscores(){
 // }
 
 //when the Start button is clicked, then start the game
-startEl.addEventListener("click", function(){
+startEl.addEventListener("click",createQuiz);
 
-    startGame();
-
-});
-
+hiscoreEl.addEventListener("click",hiscores)
